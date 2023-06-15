d53fd4f572cfdf654c45c8a994cba2e8cee20cc5
yang20002020 <2628636987@qq.com>
2023-06-15 17:25:30 +0800

    结果映射resultMap  解决字段名称不一致的问题

---
+++ Demo7/src/main/java/com/fuyu/mapper/BrandMapper.java
@@ -0,0 +1,12 @@
+package com.fuyu.mapper;
+
+import com.fuyu.pojo.Brand;
+
+import java.util.List;
+
+public interface BrandMapper {
+    /*
+    查询所有
+     */
+    public List<Brand> selectAll();
+}
