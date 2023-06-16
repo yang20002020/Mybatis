09f45169067eaca4febf46c74c3a308130551bde
yang20002020 <2628636987@qq.com>
2023-06-16 20:17:32 +0800

    动态条件查询 出现的问题

---
+++ Demo13/src/main/java/com/fuyu/mapper/BrandMapper.java
@@ -0,0 +1,36 @@
+package com.fuyu.mapper;
+
+import com.fuyu.pojo.Brand;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BrandMapper {
+    /*
+    查询所有
+     */
+    public List<Brand> selectAll();
+//    查看详情 根据id 查询
+    public Brand selectBranfById(int id );
+    /*
+       条件查询
+       参数接收
+       1. 散装参数
+        如果方法中有多个参数，需要使用@Param("sql参数占位符名称")
+
+       2.对象参数
+        对象的属性名称要和参数占位符名称一致
+
+       3. map集合参数
+
+     */
+
+
+
+    List<Brand> selectByCondition(Map map);
+
+
+
+
+}
