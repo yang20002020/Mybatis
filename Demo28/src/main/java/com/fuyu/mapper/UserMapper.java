3e45545270049a5c5b8dcd18a6830de98c664293
yang20002020 <2628636987@qq.com>
2023-06-18 15:09:23 +0800

    mybatis 参数传递 方法一

---
+++ Demo28/src/main/java/com/fuyu/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.fuyu.mapper;
+import com.fuyu.pojo.User;
+import org.apache.ibatis.annotations.Param;
+public interface UserMapper {
+/*
+    多个参数：封装成为map集合
+     map.put("arg0",参数值1)
+     map.put("param1",参数值1)
+     map.put("arg1",参数值2)
+     map.put("param2",参数值2)
+ */
+     public User select( String username, String password);
+
+}
---
+++ Demo28/src/test/java/com/fuyu/test/userTest.java
@@ -0,0 +1,40 @@
+package com.fuyu.test;
+import com.fuyu.mapper.UserMapper;
+import com.fuyu.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class userTest {
+    @Test
+    public void selectTest() throws IOException {
+
+        //1.加载mybatis的核心配置文件，获取sqlSessionFactory
+        String resource = "mybatis_config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2. 获取sqlSession对象，用它来执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //3.1 获取userMapper 接口的代理对象
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        //执行方法
+        String username="zhangsan";
+        String password="123";
+        User user = userMapper.select(username, password);
+
+        System.out.println(user);
+        //提交事务
+        sqlSession.commit();
+        //4.释放资源
+        sqlSession.close();
+    }
+
+
+}
